312237e204981dbe55ef5d5e6bef2b6d91751333
Tom Graham <61550224+tgrahamcodes@users.noreply.github.com>
2021-04-16 21:03:26 -0400

    update (everything working but last 2)

--- solution/goatfs.c
+++ solution/goatfs.c
@@ -17,11 +17,14 @@ unsigned int inodeCount;
 unsigned int blockCount;
 unsigned int inodeBlocks;
 
+/* A function used to print the status of the disk that is currently
+* mounted on the system. Prints to console, no return as void. */ 
 void debug()
 {	
-	// Start by reading the superblock
+	// Start by reading the Superblock
 	wread(0, sBlock.Data);
 
+    // Print out information about the Superblock
 	printf("SuperBlock:\n");
 	if (sBlock.Super.MagicNumber == MAGIC_NUMBER)
 	{
@@ -31,34 +34,42 @@ void debug()
 		printf("    magic number is invalid\n");
 	}
 	
-	// spacing is weird because tab set to 3 on my local, and 5 on server
+	// Tab set to 3 on my local, and 5 on server
 	printf("    %u blocks\n", sBlock.Super.Blocks);
 	printf("    %u inode blocks\n", sBlock.Super.InodeBlocks);
 	printf("    %u inodes\n", sBlock.Super.Inodes);
-	
+    
+    // Assign variables for the count and "strings".
 	unsigned int inodeCount = sBlock.Super.InodeBlocks;
 	unsigned int indirectInt = 0;
 	char *direct = NULL;
 	char *indirect = NULL;
 
-	for (unsigned int y=0; y<inodeCount; y++){
+    // Loop through the number of blocks reserved for the inodes
+	for (unsigned int y = 0; y  < inodeCount; y++){
 		wread(1+y, sBlock.Data);
-		// looping through individual inodes numbering 
-		// them by their pointer number, data in second column
+
+		// Looping through the individual Inodes
 		for (unsigned int e=0; e<INODES_PER_BLOCK; e++){
-			// printf("%u", block.Inodes[e].Size);
+            // Check that the current Inode is invalid
 			if (!sBlock.Inodes[e].Valid) {
-				// printf("%u ", sBlock.Inodes[e].Valid);
+                // And continue
                 continue;
             }
-			for (unsigned int l=0; l < POINTERS_PER_INODE; l++){
+            unsigned int l = 0;
+            // While l less than pointers in each Inode
+			while (l < POINTERS_PER_INODE){
+                // Check that the Direct pointer is not 0
 				if (sBlock.Inodes[e].Direct[l] != 0){
 					direct += ' ';
 					direct += sBlock.Inodes[e].Direct[l];
 				}
+                l++;
 			}
 			indirectInt = sBlock.Inodes[e].Indirect;
+            // Check that the indirectInt is not 0
 			if (indirectInt != 0){
+                // Then read the data segement of the block
 				wread(indirectInt, block.Data);
 				for (unsigned int j=0; j < POINTERS_PER_BLOCK; j++){
 					if (block.Pointers[j] != 0){
@@ -66,8 +77,8 @@ void debug()
 						indirect += block.Pointers[j];
 					}
 				}
-				// printf("%u ", sBlock.Inodes->Valid);
 			}
+            // Display results to console
 			printf("Inode %u:\n", e);
 			printf("    size: %u bytes\n", sBlock.Inodes[e].Size);
 			if (sBlock.Inodes[e].Valid != 0){
@@ -94,88 +105,120 @@ void debug()
 	}
 }
 
+/* A function used to format the current disk that is mounted
+* on the system. */ 
 bool format()
-{
+{   
+    // Setup variables
+    double offset = 0.5;
+    double multiplier = 0.1;
+    char flush [BUFSIZ] = {0};
+
+    // Check if any mounts were done yet, if so ret false.
 	if (_disk->Mounts > 0){
 		return false;
 	}
 
-	// printf("File Descriptor: %u", _disk->FileDescriptor);
+    // Allocate memory for the sBlock Data segment
     memset(sBlock.Data,0,BLOCK_SIZE);
+
 	sBlock.Super.MagicNumber = MAGIC_NUMBER;
     sBlock.Super.Blocks = _disk->Blocks;
-    sBlock.Super.InodeBlocks = (size_t)(((float)_disk->Blocks*0.1)+0.5);
-    sBlock.Super.Inodes = INODES_PER_BLOCK*sBlock.Super.InodeBlocks;
+
+    sBlock.Super.InodeBlocks = (((double) _disk->Blocks *multiplier)+offset);
+    sBlock.Super.Inodes = INODES_PER_BLOCK * sBlock.Super.InodeBlocks;
+
+    // Finally write to the 0 block
    	wwrite(0, sBlock.Data);
 
-  
-    char clear[BUFSIZ] = {0};
-    for (size_t i=1; i<sBlock.Super.Blocks; i++) {
-        wwrite(i, clear);
+    // Flush the buffer 
+    for (unsigned int i = 1; i < sBlock.Super.Blocks; i++) {
+        wwrite(i, flush);
     }
 
 	return true;
 }
 
+/* A function used to mount the provided disk block to the system. */ 
 int mount()
 {
+    // Check if there have already been mounts, ret -1 if so
 	if (_disk->Mounts != 0){
 		return -1;
 	}
+
+    // Setup variables
 	Block mountSb;
+    double offset = 0.1;
+
+    // Read into mountSb.Data
 	wread(0, mountSb.Data);
 
+    // Multiple bad disk error checks
 	if (mountSb.Super.MagicNumber != MAGIC_NUMBER){
 		return -1;
 	}
-
     if (_disk->Blocks != mountSb.Super.Blocks){
         return -1;
     }
-
+    if (mountSb.Super.InodeBlocks != ceil(offset * mountSb.Super.Blocks)) {
+        return -1;
+    } 
 	if (mountSb.Super.Blocks < 0){
 		return -1;
 	}
-
 	if (mountSb.Super.Inodes != (mountSb.Super.InodeBlocks * INODES_PER_BLOCK)){
 		return -1;
 	}
-	if (mountSb.Super.InodeBlocks != ceil(.1 * mountSb.Super.Blocks)) {
-        return -1;
-    } 
-
+	
+    // Setup variables for use later in the project
 	blockCount = mountSb.Super.Blocks;
+
 	inodeCount = mountSb.Super.Inodes;
+
 	inodeBlocks = mountSb.Super.InodeBlocks;
 
-	memset(FREE_MAP,0,BLOCK_SIZE);
+    // Allocate memory for our FREE_MAP bitmap disk image
+	memset(FREE_MAP, 0, BLOCK_SIZE);
 
-	// printf("FREE_MAP = %u \n", (unsigned int)sizeof(FREE_MAP));
+    // Set FREE_MAP initally to 1
 	for (unsigned int i=0; i < blockCount; i++){
 		FREE_MAP[i] = 1;
 	}
 
+    // Superblock is 0 
 	FREE_MAP[0] = 0;
 
+    // Then set FREE_MAP indices + 1 to 0
 	for (unsigned int j=0; j < inodeBlocks; j++){
 		FREE_MAP[j+1] = 0;
 	}
 	
+    // Create another block
     Block mountB;
-	for (unsigned int x=0; x < inodeBlocks; x++){
+
+    // Loop through the inode blocks and read incrementing by one each time
+	for (unsigned int x = 0; x < inodeBlocks; x++){
 		wread(x+1, mountB.Data);
 		
-		for (unsigned int n=0; n < INODES_PER_BLOCK; n++){
-			if(!mountB.Inodes[n].Valid){
+        // Loop again through the Inodes per Block
+		for (unsigned int n = 0; n < INODES_PER_BLOCK; n++){
+
+            // If current Inode isn't valid continue on
+			if(mountB.Inodes[n].Valid == 0){
 				continue;
 			}
-			unsigned int b = ceil(mountB.Inodes[n].Size/(double)BLOCK_SIZE);
+            
+            // Set up a variable for conditional checking pointers per node 
+			unsigned int b = ceil(mountB.Inodes[n].Size/BLOCK_SIZE);
 			
-			for (unsigned int e=0; e < POINTERS_PER_INODE && e < b; e++){
+            // Loop through the pointers per Inode setting the FREE_MAP to 0
+			for (unsigned int e=  0; e < POINTERS_PER_INODE && e < b; e++){
 				FREE_MAP[mountB.Inodes[n].Direct[e]] = 0;
 			}
 
 			if (b > POINTERS_PER_INODE){
+                // Use another block for the indirect data segment
 				Block indB;
 				wread(mountB.Inodes[n].Indirect, indB.Data);
 				FREE_MAP[mountB.Inodes[n].Indirect] = 0;
@@ -185,6 +228,7 @@ int mount()
 			}
 		}
 	}
+    // Increase number of disk mounts
     _disk->Mounts++;
 	return 0;
 }
@@ -197,22 +241,34 @@ void initializeInode(Inode *node) {
     node->Size      = 0;
 }
 
+/* A function used to create the disk block and add it to the system. */ 
 ssize_t create() {
-    // Locate free inode in inode table
+    // Start with an inodenumber of -1
     ssize_t inodeNumber = -1;
-    for (uint32_t i = 0; i < INODES_PER_BLOCK; i++) {
-		uint32_t x = 0;
+
+    // Loop through inodes per block
+    for (unsigned int i = 0; i < INODES_PER_BLOCK; i++) {
+
+        // Setup variables
+		unsigned int x = 0;
         Block inodeBlock;
-        wread(x+1, inodeBlock.Data);
-        for (uint32_t j = 0; j < INODES_PER_BLOCK + 1; j++){
-            if (!inodeBlock.Inodes[j].Valid){
+
+        // Read into inodeBlock increase by 1 each iteration
+        wread(x + 1, inodeBlock.Data);
+
+        for (unsigned int j = 0; j < INODES_PER_BLOCK + 1; j++){
+
+            if (inodeBlock.Inodes[j].Valid == 0){
                 inodeBlock.Inodes[j].Valid = 1;
                 inodeNumber = j+INODES_PER_BLOCK*i;
-				if (inodeNumber > 128){
+
+
+				if (inodeNumber > INODES_PER_BLOCK){
 					inodeNumber = -1;
 					break;
 				}
-                initializeInode(&inodeBlock.Inodes[j]);
+
+                // Write to the inode Data segment then break
                 wwrite(x+1, inodeBlock.Data);
                 break;
             }
@@ -222,14 +278,16 @@ ssize_t create() {
         }
 		x++;
 	}
-
     return inodeNumber;
 }
 
+/* A function used to remove the disk from the system. */ 
 bool wremove(size_t inumber)
 {
+    // Setup variables
 	Inode inode;
 
+    // Check that disk is valid using a helper function
     if (loadInode(inumber, &inode) == false) {
         return false;
     }
@@ -243,21 +301,29 @@ bool wremove(size_t inumber)
             inode.Direct[i] = 0;
         }
     }
-    
+
     FREE_MAP[inode.Indirect] = 1;
+
+    // Setup another block for the indirect segment
 	Block iBlock;
+
+    // Read into the iBlock.Data
     wread(inode.Indirect, iBlock.Data);
 
+    // Reset the properties of inode to 0
    	inode.Indirect = 0;
     inode.Valid = 0;
     inode.Size = 0;
-    if (saveInode(inumber, &inode) == 0) {
+
+    // Try to save inode, if false ret false
+    if (saveInode(inumber, &inode) == false) {
         return false;
     }
 
     return true;
 }
 
+/* A function used to test the validity of the inode passed by its number. */ 
 ssize_t stat(size_t inumber)
 {
 	Inode inode;
@@ -268,6 +334,7 @@ ssize_t stat(size_t inumber)
     return inode.Size;
 }
 
+/* A function used to read the disk block and add it to the system. */ 
 ssize_t wfsread(size_t inumber, char *data, size_t length, size_t offset)
 {
     if (inumber >= inodeCount){
@@ -279,7 +346,6 @@ ssize_t wfsread(size_t inumber, char *data, size_t length, size_t offset)
     if (!i) { 
         return -1; 
     }
-
     if (inode.Size == offset) {
         return -1; 
     }
@@ -316,7 +382,7 @@ ssize_t wfsread(size_t inumber, char *data, size_t length, size_t offset)
     if (inode.Valid){
         inode.Indirect = true;
     }
-    if (readIndex && inode.Indirect){
+    if (readIndex){
         wread(inode.Indirect, indirectBlock.Data);
         startBlock = startBlock - POINTERS_PER_INODE;
         while (startBlock < POINTERS_PER_BLOCK){
@@ -329,6 +395,12 @@ ssize_t wfsread(size_t inumber, char *data, size_t length, size_t offset)
                 if (indirectBlock.Inodes->Valid == 0){
                     continue;
                 }
+                printf("The loop: %u ", indirectBlock.Pointers[startBlock]);
+            }
+
+           // printf("Read Index: %u\nIndirectBlock Pointer: %u\n", readIndex, indirectBlock.Pointers[startBlock]);
+            if (indirectBlock.Pointers[startBlock] > sizeof(indirectBlock.Pointers)){
+                indirectBlock.Pointers[startBlock] = 0;
             }
             wread(indirectBlock.Pointers[startBlock], wfsB.Data);
             unsigned int blockSizeVar = BLOCK_SIZE;
@@ -350,11 +422,12 @@ ssize_t wfsread(size_t inumber, char *data, size_t length, size_t offset)
     return dataIndex;
 }
 
+/* A function used to write to the disk block and add it to the system. */ 
 ssize_t wfswrite(size_t inumber, char *data, size_t length, size_t offset)
 {
 	Inode inode;
     if (!loadInode(inumber, &inode) || offset > inode.Size) {
-        // printf("debug 1: ");
+        printf("debug 1: ");
         return -1;
     }
 
@@ -428,7 +501,7 @@ ssize_t wfswrite(size_t inumber, char *data, size_t length, size_t offset)
         written += write_length;
     }
 
-    uint32_t new_size = (size_t)inode.Size;
+    uint32_t new_size = (size_t) inode.Size;
 	if (new_size < (written + offset)){
 		new_size = (written + offset);
 	}else if ((new_size < inode.Size)){
@@ -441,11 +514,11 @@ ssize_t wfswrite(size_t inumber, char *data, size_t length, size_t offset)
     }
 
     if (modified_inode) {
-        saveInode(inumber,&inode);
+        saveInode(inumber, &inode);
     }
      
     if (modified_indirect) {
-        wwrite(inode.Indirect,indirect.Data);
+        wwrite(inode.Indirect, indirect.Data);
     }
 
     return written;
@@ -471,14 +544,14 @@ bool loadInode(ssize_t inumber, Inode *inode) {
 }
 
 bool saveInode(ssize_t inumber, Inode *inode) {
-
     size_t blockNum = 1 + (inumber / INODES_PER_BLOCK);
     size_t offset = inumber % INODES_PER_BLOCK;
 
     if (inumber >= inodeCount) {
         return false;
     }
-	Block saveB;
+	
+    Block saveB;
     wread(blockNum, saveB.Data);
     saveB.Inodes[offset] = *inode;
     wwrite(blockNum, saveB.Data);
